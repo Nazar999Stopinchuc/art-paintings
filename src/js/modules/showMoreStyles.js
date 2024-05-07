04421630dc2da384250aa887a8c9fda1c1113080
Nazar999Stopinchuc <dgvfffbhf@gmail.com>
2024-05-06 20:14:43 -0400

    first commit

---
+++ src/js/modules/showMoreStyles.js
@@ -0,0 +1,33 @@
+import { getResource } from "../services/requests";
+
+const showMoreStyles = (trigger, wrapper) => {
+  const btn = document.querySelector(trigger);
+
+  btn.addEventListener('click', function () {
+    getResource('assets/db.json')
+      .then(res => createCards(res.styles))
+      .catch(error => console.log(error));
+
+    this.remove();
+  });
+
+  function createCards(response) {
+    response.forEach(item => {
+      let card = document.createElement('div');
+
+      card.classList.add('animated', 'fadeInUp', 'col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+
+      card.innerHTML = `
+          <div class=styles-block>
+						<img src=${item.src} alt="style">
+						<h4>${item.title}</h4>
+						<a href="${item.link}">Подробнее</a>
+					</div>
+        `;
+
+      document.querySelector(wrapper).appendChild(card);
+    });
+  };
+
+};
+export default showMoreStyles;
